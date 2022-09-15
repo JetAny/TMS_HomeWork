f938cab4321df31fbcfe1716ff7919913c995ff8
JetAny <lebany45@gmail.com>
2022-09-15 13:22:41 +0300

    Шаблон

---
+++ HM_23/DBgarage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DBgarage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
